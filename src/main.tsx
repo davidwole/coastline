2e842d3f33744eb8a260c13b2c1084a9929a5fd8
davidwole <davidwole20@gmail.com>
2026-01-05 05:15:55 +0100

    clear unused variable

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import Header from "./components/Header.tsx";
 import Hero from "./components/Hero.tsx";
 import Body from "./components/Body.tsx";
